300a1a8ef57e4a995380ef95f115c30e260534a5
royeradames <royeraadames@gmail.com>
2021-05-17 22:03:27 -0400

    📦group heading with body, and baner with form

--- src/App.tsx
+++ src/App.tsx
@@ -89,33 +89,36 @@ function App() {
 
   return (
     <main className="sign-up-form">
-      <h1 className="sign-up-form__heading">
-        Learn to code by watching others
-      </h1>
-      <p className="sign-up-form__body">
-        See how experienced developers solve problems in real-time. Watching
-        scripted tutorials is great, but understanding how developers think is
-        invaluable.
-      </p>
-      <div className="sign-up-form__banner">
-        Try it free 7 days then $20/mo. thereafter
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="sign-up-form__form">
-        {
-          // render the inputs
-          formInputs()
-        }
-
-        <button className="sign-up-form__btn">CLAIM YOUR FREE TRIAL</button>
-        <p className="sign-up-form__terms">
-          By clicking the button, you are agreeing to our
-          <a href="#">
-            <span className="sign-up-form__terms--link">
-              Terms and Services
-            </span>
-          </a>
+      <div className="sign-up-form__call-to-action">
+        <h1 className="sign-up-form__heading">
+          Learn to code by watching others
+        </h1>
+        <p className="sign-up-form__body">
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
         </p>
-      </form>
+      </div>
+      <div className="sign-up-form__listing">
+        <p className="sign-up-form__banner">
+          Try it free 7 days then $20/mo. thereafter
+        </p>
+        <form onSubmit={handleSubmit(onSubmit)} className="sign-up-form__form">
+          {
+            // render the inputs
+            formInputs()
+          }
+          <button className="sign-up-form__btn">CLAIM YOUR FREE TRIAL</button>
+          <p className="sign-up-form__terms">
+            By clicking the button, you are agreeing to our
+            <a href="#">
+              <span className="sign-up-form__terms--link">
+                Terms and Services
+              </span>
+            </a>
+          </p>
+        </form>
+      </div>
     </main>
   );
 }
